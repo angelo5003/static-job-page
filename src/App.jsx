784c889d80c4c38309da145515dcb34d4dae1aa3
Tony5300 <tony_tolen@ziggo.nl>
2023-11-28 16:46:28 +0100

    feat: added the filter on click function. If you click now on one of the tags, it will show all the items that have the same tags.

--- src/App.jsx
+++ src/App.jsx
@@ -5,18 +5,56 @@ import JobCard from "./components/JobCard/JobCard";
 
 function App() {
   const [data, setData] = useState(jsonData);
-  console.log(`data in json:`, data);
+  const [selectedTags, setSelectedTags] = useState([]);
+
+  const handleSelectTags = (tag) => {
+    // Check if the tag is already selected
+    if (selectedTags.includes(tag)) {
+      setSelectedTags(
+        // If it is selected, remove it from the selected tags array
+        selectedTags.filter((selectedTag) => selectedTag !== tag)
+      );
+    } else {
+      // If it is not selected, add it to the selected tags array
+      setSelectedTags([...selectedTags, tag]);
+    }
+  };
+
+  const filteredJobs = data.filter((jobData) => {
+    // Check if any of the selected tags are present in languages or tools or role or level
+    // if there is no math
+    return (
+      selectedTags.some(
+        // Using the some method to iterate through selectedTags
+        // The some method returns true if the condition is met for at least one tag
+        (tag) =>
+          jobData.languages.includes(tag) ||
+          jobData.tools.includes(tag) ||
+          jobData.role.includes(tag) ||
+          jobData.level.includes(tag)
+      ) ||
+      // If there are no selected tags (length of selectedTags is zero), return the entire array and show all the items on the DOM
+      selectedTags.length === 0
+    );
+  });
 
   return (
     <>
       <header className="header-background-image-container">
         <img className="header-background" />
       </header>
+
       <div className="body-container">
         {Array.isArray(data) ? (
           // If there is data and the data is an array than map
-          data.map((jobData) => {
-            return <JobCard jobData={jobData} key={jobData.id} />;
+          filteredJobs.map((jobData) => {
+            return (
+              <JobCard
+                jobData={jobData}
+                key={jobData.id}
+                handleSelectTag={handleSelectTags}
+              />
+            );
           })
         ) : (
           // if there is no data or the data is not an array than show this error
--- src/components/JobCard/JobCard.jsx
+++ src/components/JobCard/JobCard.jsx
@@ -1,43 +1,77 @@
 import "./JobCardStyle.css";
 import { jobDataPropTypes } from "../../utils/PropValidation/PropValidation";
 
-const JobCard = ({ jobData }) => {
+const JobCard = ({ jobData, handleSelectTag }) => {
   return (
-    <section className="card-outer-container">
-      <div className="card-container">
-        <div className="card-company-logo-container">
-          <img
-            src={jobData.logo}
-            alt="company-logo"
-            className="card-company-logo"
-          />
+    <>
+      <section className="card-outer-container">
+        <div className="card-container">
+          <div className="card-company-logo-container">
+            <img
+              src={jobData.logo}
+              alt="company-logo"
+              className="card-company-logo"
+            />
+          </div>
+          <section className="card-inner-container">
+            <ul className="card-header-info">
+              <li className="card-company-name">{jobData.company}</li>
+              <li className={jobData.new ? "card-new-tag" : null}>
+                {jobData.new ? "new!".toUpperCase() : ""}
+              </li>
+              <li className={jobData.featured ? "card-featured-tag" : null}>
+                {jobData.featured ? "featured".toUpperCase() : ""}
+              </li>
+            </ul>
+            <p className="card-job-position">{jobData.position}</p>
+            <ul className="card-job-details">
+              <li className="job-details">{jobData.postedAt}</li>
+              <li className="job-details">{jobData.contract}</li>
+              <li className="job-details">{jobData.location}</li>
+            </ul>
+            <hr />
+            <ul className="card-job-work-info">
+              {/* <div className="test"> */}
+              <li
+                className="job-info"
+                onClick={() => handleSelectTag(jobData.role)}
+              >
+                {jobData.role}
+              </li>
+              <li
+                className="job-info"
+                onClick={() => handleSelectTag(jobData.level)}
+              >
+                {jobData.level}
+              </li>
+              {jobData.languages.map((lang, id) => {
+                return (
+                  <li
+                    key={id}
+                    className="job-info"
+                    onClick={() => handleSelectTag(lang)}
+                  >
+                    {lang}
+                  </li>
+                );
+              })}
+              {jobData.tools.map((tool, id) => {
+                return (
+                  <li
+                    key={id}
+                    className="job-info"
+                    onClick={() => handleSelectTag(tool)}
+                  >
+                    {tool}
+                  </li>
+                );
+              })}
+              {/* </div> */}
+            </ul>
+          </section>
         </div>
-        <section className="card-inner-container">
-          <ul className="card-header-info">
-            <li className="card-company-name">{jobData.company}</li>
-            <li className={jobData.new ? "card-new-tag" : null}>
-              {jobData.new ? "new!".toUpperCase() : ""}
-            </li>
-            <li className={jobData.featured ? "card-featured-tag" : null}>
-              {jobData.featured ? "featured".toUpperCase() : ""}
-            </li>
-          </ul>
-          <p className="card-job-position">{jobData.position}</p>
-          <ul className="card-job-details">
-            <li className="job-details">{jobData.postedAt}</li>
-            <li className="job-details">{jobData.contract}</li>
-            <li className="job-details">{jobData.location}</li>
-          </ul>
-          <hr />
-          <ul className="card-job-work-info">
-            <li>{jobData.role}</li>
-            <li>{jobData.level}</li>
-            <li>{jobData.languages}</li>
-            <li>{jobData.tools}</li>
-          </ul>
-        </section>
-      </div>
-    </section>
+      </section>
+    </>
   );
 };
 
